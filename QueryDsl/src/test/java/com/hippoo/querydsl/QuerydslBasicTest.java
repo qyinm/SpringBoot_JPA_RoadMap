7573af37184e45a0d34174a77213546510da22a9
qyinm <qusseun@gmail.com>
2025-04-16 23:35:54 +0900

    test: queryDsl join on

--- QueryDsl/src/test/java/com/hippoo/querydsl/QuerydslBasicTest.java
+++ QueryDsl/src/test/java/com/hippoo/querydsl/QuerydslBasicTest.java
@@ -268,4 +268,53 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    @DisplayName("")
+    void join_on() {
+        // When
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+//                .join(member.team, team)
+                .on(team.name.eq("teamA"))  // left join 일 때 on 사용
+//                .where(team.name.eq("teamA")) // inner join 일 때는 where 사용하나 on 사용하나 동일함.
+                .fetch();
+
+        // Then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    @DisplayName("")
+    void join_on_no_relation() {
+        // Given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        // When
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        // Then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
