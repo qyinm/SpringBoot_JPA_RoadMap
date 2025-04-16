5c8e914fd7ea63039617db3b52d797a6c165e580
qyinm <qusseun@gmail.com>
2025-04-16 22:40:07 +0900

    test: queryDsl sort

--- QueryDsl/src/test/java/com/hippoo/querydsl/QuerydslBasicTest.java
+++ QueryDsl/src/test/java/com/hippoo/querydsl/QuerydslBasicTest.java
@@ -105,4 +105,34 @@ public class QuerydslBasicTest {
                 .fetchCount();
         
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 올림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        // given
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        // when
+        List<Member> memberList = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        // then
+        Member member5 = memberList.get(0);
+        Member member6 = memberList.get(1);
+        Member memberNull = memberList.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
 }
